49c5a6858eb14fc807028a72a223d159c1e87488
Sanjay <sk006088@gmail.com>
2022-04-01 17:28:38 +0530

    react added

--- hotelbookingreact/src/Components/Carousel.js
+++ hotelbookingreact/src/Components/Carousel.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import pic1 from './Images/1.jpg';
-import pic2 from './Images/2.jpg';
-import pic3 from './Images/3.jpg';
+import pic1 from '../Images/1.jpg';
+import pic2 from '../Images/2.jpg';
+import pic3 from '../Images/3.jpg';
 
 
 export default function Carousel() {
-    return (
+  return (
 
-        <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-              <img src={pic1} className="d-block w-100" alt="..."/>
+    <div className='contriner'>
+
+      <div class="bd-example">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src={pic1} class="d-block w-100" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={pic1} class="d-block w-100" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={pic1} class="d-block w-100" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </div>
+            </div>
           </div>
-          <div className="carousel-item">
-            <img src={pic2} className="d-block w-100" alt="..."/>
-          </div>
-          <div className="carousel-item">
-            <img src={pic3} className="d-block w-100" alt="..."/>
-          </div>
+          <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
       </div>
-
-
-    )
+      
+    </div>
+  )
 }
