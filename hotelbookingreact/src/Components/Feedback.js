68130ea246ab4babfa047db43dd74181f68cff56
Amit <rs150694@gmail.com>
2022-04-01 21:29:23 +0530

    update

---
+++ hotelbookingreact/src/Components/Feedback.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Components/Feedback.css'
+
+export default function Feedback() {
+    return (
+        <>
+        <div className='container'>
+        <div className="feedback">
+            <h1 className='text'>Your Feedback is important to us.</h1>
+            <input type="textarea" className="input-text" ></input>
+        </div>
+        </div>
+        </>
+
+    )
+}
