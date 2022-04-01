79c06c84bfe7c8b15138d4bd50986c4809fe8af2
Sanjay <sk006088@gmail.com>
2022-04-01 16:42:06 +0530

    update

--- hotelbookingreact/src/App.js
+++ hotelbookingreact/src/App.js
@@ -2,15 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Searchbar from './Components/Searchbar';
+import Carousel from './Components/Carousel';
 
 function App() {
   return (
     <>
-    <div>
-    <Navbar/></div>
-    <div>
-    <Searchbar/></div>
-      
+    <Navbar/>
+    <Searchbar/>
     </>
   );
 }
--- hotelbookingreact/src/index.js
+++ hotelbookingreact/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './Components/Navbar';
+import Searchbar from './Components/Searchbar';
 
 ReactDOM.render(
   <React.StrictMode>
