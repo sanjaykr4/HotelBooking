db32cb40008842be0ee84814d716d7b52bf38153
Sanjay <sk006088@gmail.com>
2022-04-03 14:01:47 +0530

    routing updated

---
+++ src/Components/Searchbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Searchbar() {
+    return (
+        <div className='container my-5'>
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Enter City Name" />
+                <div className="input">
+                    <button type="button" className="btn btn-primary btn-lg mx-3">Search</button>
+                </div>
+            </div>
+        </div>
+    )
+}
