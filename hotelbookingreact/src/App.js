624e33898e0941ee49efacc80ffbbc1b38eeda2e
Sanjay <sk006088@gmail.com>
2022-04-03 14:18:26 +0530

    routing updated

--- hotelbookingreact/src/App.js
+++ hotelbookingreact/src/App.js
@@ -1,18 +1,31 @@
+import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Searchbar from './Components/Searchbar';
 import Carousel from './Components/Carousel';
-import Feedback from './Components/Feedback';
 import Footer from './Components/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Searchbar/>
-    <Carousel/>
-    <Feedback/>
-    <Footer/>
+      <Router>
+        <Navbar />
+        <Searchbar />
+
+        <Routes>
+          <Route exact path='/' element={<Carousel/>}/>
+          <Route exact path='/about' element={<Footer />}/>
+          </Routes>
+
+      </Router>
     </>
   );
 }
